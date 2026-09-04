43af1df5cb2483e216af70cf4c66cfe94cc4f800
zafrandroidgo <zafranstb860@gmail.com>
2026-09-04 20:20:28 +0700

    menu.sh

--- menu.sh
+++ menu.sh
@@ -1,5 +1,4 @@
 #!/system/bin/sh
-# URL dasar repository GitHub Anda
 REPO_URL="https://raw.githubusercontent.com/zafrandroidgo/zafran-repo/main"
 
 while true; do
@@ -13,28 +12,26 @@ while true; do
     echo -n "Pilih menu [1-2]: "
     read pilihan_utama
 
-    case $pilihan_utama in
+    case "$pilihan_utama" in
         1)
             while true; do
                 clear
                 echo "=================================="
                 echo "      PILIH APLIKASI UTK INSTAL   "
                 echo "=================================="
-                echo "1. Zafran APK (Aplikasi Utama)"
-                echo "2. Aplikasi Kedua (Contoh: YouTube Vanced/Lainnya)"
-                echo "3. Aplikasi Ketiga"
-                echo "4. Kembali ke Menu Utama"
+                echo "1. Adiryu APK"
+                echo "2. Kembali ke Menu Utama"
                 echo "----------------------------------"
-                echo -n "Pilih aplikasi [1-4]: "
+                echo -n "Pilih aplikasi [1-2]: "
                 read pilihan_apk
 
-                case $pilihan_apk in
+                case "$pilihan_apk" in
                     1)
-                        echo "Mengunduh Zafran APK..."
-                        curl -L -o /data/local/tmp/adiryu.apk "https://github.com/zafrandroidgo/zafran-repo/releases/download/v1.0.0/adiryu.apk"
+                        echo "Mengunduh Adiryu APK..."
+                        wget -O /data/local/tmp/adiryu.apk "https://github.com/zafrandroidgo/zafran-repo/releases/download/v1.0.0/adiryu.apk"
                         if [ $? -eq 0 ]; then
-                            echo "Menginstall Zafran APK..."
-                            pm install -r /data/local/tmp/zafran.apk
+                            echo "Menginstall Adiryu APK..."
+                            pm install -r /data/local/tmp/adiryu.apk
                             echo "Instalasi Selesai!"
                         else
                             echo "Gagal mengunduh! Periksa koneksi."
@@ -44,37 +41,6 @@ while true; do
                         read enter
                         ;;
                     2)
-                        echo "Mengunduh Aplikasi Kedua..."
-                        # Ganti 'appkedua.apk' dengan nama file apk kedua Anda di GitHub
-                        curl -L -o /data/local/tmp/appkedua.apk "$REPO_URL/appkedua.apk"
-                        if [ $? -eq 0 ]; then
-                            echo "Menginstall Aplikasi Kedua..."
-                            pm install -r /data/local/tmp/appkedua.apk
-                            echo "Instalasi Selesai!"
-                        else
-                            echo "Gagal mengunduh! Periksa koneksi."
-                        fi
-                        echo ""
-                        echo "Tekan Enter untuk melanjutkan..."
-                        read enter
-                        ;;
-                    3)
-                        echo "Mengunduh Aplikasi Ketiga..."
-                        # Ganti 'appketiga.apk' dengan nama file apk ketiga Anda di GitHub
-                        curl -L -o /data/local/tmp/appketiga.apk "$REPO_URL/appketiga.apk"
-                        if [ $? -eq 0 ]; then
-                            echo "Menginstall Aplikasi Ketiga..."
-                            pm install -r /data/local/tmp/appketiga.apk
-                            echo "Instalasi Selesai!"
-                        else
-                            echo "Gagal mengunduh! Periksa koneksi."
-                        fi
-                        echo ""
-                        echo "Tekan Enter untuk melanjutkan..."
-                        read enter
-                        ;;
-                    4)
-                        # Keluar dari sub-menu kembali ke menu utama
                         break
                         ;;
                     *)

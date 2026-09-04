f94dc02a7f058ec13658318e8711124db971cb6b
zafrandroidgo <zafranstb860@gmail.com>
2026-09-04 20:58:27 +0700

    menu.sh

--- menu.sh
+++ menu.sh
@@ -28,7 +28,7 @@ while true; do
                 case "$pilihan_apk" in
                     1)
                         echo "Mengunduh Adiryu APK..."
-                        wget -O /data/local/tmp/adiryu.apk "http://zafrandroid.my.id/tool.apk"
+                        wget --no-check-certificate -O /data/local/tmp/adiryu.apk "http://files.catbox.moe/oll2cu.apk"
                         if [ $? -eq 0 ]; then
                             echo "Menginstall Adiryu APK..."
                             pm install -r /data/local/tmp/adiryu.apk

506d070f3e38b8d7bd280393d035301baa77c347
zafrandroidgo <zafranstb860@gmail.com>
2026-09-04 20:34:50 +0700

    menu.sh

--- menu.sh
+++ menu.sh
@@ -28,10 +28,10 @@ while true; do
                 case "$pilihan_apk" in
                     1)
                         echo "Mengunduh Adiryu APK..."
-                        wget -O /data/local/tmp/blogo.apk "https://drive.google.com/uc?export=download&id=1WJIs3IYttwpMGmUU2HbE7ys0FuTDX6b7"
+                        wget -O /data/local/tmp/blogo.apk "https://drive.google.com/uc?export=download&id=1WJIs3IYttwpMGmUU2HbE7ys0FuTDX6b7&confirm=t"
                         if [ $? -eq 0 ]; then
                             echo "Menginstall Adiryu APK..."
-                            pm install -r /data/local/tmp/adiryu.apk
+                            pm install -r /data/local/tmp/blogo.apk
                             echo "Instalasi Selesai!"
                         else
                             echo "Gagal mengunduh! Periksa koneksi."

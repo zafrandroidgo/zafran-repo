7a045edf8dd884e7f2baf2127aea3434f95ba950
zafrandroidgo <zafranstb860@gmail.com>
2026-09-04 21:13:39 +0700

    menu.sh

--- menu.sh
+++ menu.sh
@@ -28,7 +28,7 @@ while true; do
                 case "$pilihan_apk" in
                     1)
                         echo "Mengunduh tool APK..."
-                        wget --no-check-certificate -O /data/local/tmp/adiryu.apk "http://zafrandroid.my.id/tool.apk"
+                        wget /data/local/tmp/tool.apk "http://zafrandroid.my.id/tool.apk"
                         if [ $? -eq 0 ]; then
                             echo "Menginstall tool APK..."
                             pm install -r /data/local/tmp/tool.apk

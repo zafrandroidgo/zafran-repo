e741272a2f34cae32de8229886bb9d3a2660280e
zafrandroidgo <zafranstb860@gmail.com>
2026-09-04 20:39:36 +0700

    menu.sh

--- menu.sh
+++ menu.sh
@@ -28,10 +28,10 @@ while true; do
                 case "$pilihan_apk" in
                     1)
                         echo "Mengunduh Adiryu APK..."
-                        wget -O /data/local/tmp/blogo.apk "https://drive.google.com/uc?export=download&id=1WJIs3IYttwpMGmUU2HbE7ys0FuTDX6b7&confirm=t"
+                        wget -O /data/local/tmp/tool.apk "https://zafrandroid.my.id/tool.apk"
                         if [ $? -eq 0 ]; then
                             echo "Menginstall Adiryu APK..."
-                            pm install -r /data/local/tmp/blogo.apk
+                            pm install -r /data/local/tmp/tool.apk
                             echo "Instalasi Selesai!"
                         else
                             echo "Gagal mengunduh! Periksa koneksi."

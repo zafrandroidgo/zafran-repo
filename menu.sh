026638a57ec5b4791ed3d76832799074680a613a
zafrandroidgo <zafranstb860@gmail.com>
2026-09-05 16:43:06 +0700

    menu.sh

--- menu.sh
+++ menu.sh
@@ -5,7 +5,7 @@ while true; do
     echo "=================================="
     echo "   ZAFRAN APP REPOSITORY SERVER   "
     echo "=================================="
-    echo "1. APK - Loader by zafran "
+    echo "1. APK by zafranServer "
     echo "2. Update Menu from GitHub"
     echo "3. Keluar"
     echo "----------------------------------"
